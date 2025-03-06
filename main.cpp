43d6ddf544a32f33a3af936199a0340fd93f5706
matthes <matthesboettcher03@gmail.com>
2025-03-06 19:06:20 +0100

    Funktionalitaeten eingebaut, wie einen Zustandswechsel zum Einstellen der Funktionalitaeten

--- main.cpp
+++ main.cpp
@@ -1,97 +1,118 @@
-#define F_CPU 1000000UL  // Taktfrequenz des Mikrocontrollers
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
+#include <util/delay.h>
 
-uint8_t MIN_BR = 250;
-uint8_t HOUR_BR = 250;
+bool IN_STANDARD_MODI = true;
 
 volatile uint8_t seconds = 0;
 volatile uint8_t minutes = 0;
 volatile uint8_t hours = 0;
 
-volatile uint8_t previousRisingEdge_pb0 = 0;
-volatile uint8_t previousRisingEdge_pb1 = 0;
-volatile uint8_t previousRisingEdge_pb2 = 0;
+volatile bool PB0_ps = false;
+volatile bool PB1_ps = false;
+volatile bool PB2_ps = false;
+
+volatile bool PB0_cs = false;
+volatile bool PB1_cs = false;
+volatile bool PB2_cs = false;
+
+volatile uint8_t btnPressedTimer = 0;
+
+uint8_t MIN_BR = 254;
+uint8_t HOUR_BR = 254;
 
 void handleTimeChange();
-void handleMinutes();
 void handleHours();
 
-void refreshLEDs_min();
-void refreshLEDs_hours();
+void initPWM() {
+    // fast pwm, nicht invertierend
+    TCCR0A |= (1 << WGM00) | (1 << WGM01); // Fast PWM
+    TCCR0A |= (1 << COM0A1); // Nicht-invertierend auf OC0A
+    TCCR0A |= (1 << COM0B1); // Nicht-invertierend auf OC0A
+    TCCR0B |= (1 << CS01) | (1 << CS00);  // Prescaler 64
+    OCR0A = MIN_BR;
+    OCR0B = HOUR_BR;
+}
 
-void goToSleep();
+// Zeitbasis 1s
+void initQuartz() {
+    ASSR |= (1 << AS2); // Uhrenquarz asynchron zum CPU takt laufen lassen
+    TCCR2B |= (1 << CS22) | (1 << CS20); // ps 128
+    TIMSK2 |= (1 << TOIE2);
+}
 
-void init_buttons() {
+void initGPIO() {
+    DDRC |= 0b00111111; // minuten
+    DDRD |= 0b11111111; // stunden
+}
+
+void initButtons() {
+    PCICR |= (1<<PCIE0); // controll register für PD7:0 aktivieren
+    PCIFR |= (1<<PCIF0);
+
+    // PCINT0 bzw. Pull-Up aktivieren
+    DDRB &= ~(1<<PB0);
+    PORTB |= (1<<PB0);
+    // interrupt
+    PCMSK0 |= (1<<PCINT0);
+
     // PCINT1 bzw. Pull-Up aktivieren
     DDRB &= ~(1<<PB1);
     PORTB |= (1<<PB1);
     // interrupt
-    PCICR |= (1<<PCIE0); // control register
     PCMSK0 |= (1<<PCINT1); // mask aktivieren
 
     // PCINT2 bzw. Pull-Up aktivieren
     DDRB &= ~(1<<PB2);
     PORTB |= (1<<PB2);
     // interrupt
-    PCICR |= (1<<PCIE0);
     PCMSK0 |= (1<<PCINT2);
-
-    // PCINT0 bzw. Pull-Up aktivieren
-    DDRB &= ~(1<<PB0);
-    PORTB |= (1<<PB0);
-    // interrupt
-    PCICR |= (1<<PCIE0);
-    PCMSK0 |= (1<<PCINT0);
 }
 
-// Zeitbasis 1s
-void init_timer() {
-    ASSR |= (1 << AS2);
-    TCCR2B |= (1 << CS22) | (1 << CS20); // ps 128
-    TIMSK2 |= (1 << TOIE2);
-    while (ASSR & ((1 << TCR2BUB) | (1 << OCR2BUB) | (1 << TCN2UB)));
+void initSleep() {
+    set_sleep_mode(SLEEP_MODE_IDLE);
+    sleep_enable();
+    sleep_cpu();
+    // CPU wakes up here after an interrupt
+    sleep_disable(); // Disable sleep mode after wake-up
 }
 
-// LED Helligkeit
-void initPWM() {
-    DDRD |= (1 << PD6);  // OC0A als Ausgang
-    // Fast PWM, nicht-invertierend
-    TCCR0A |= (1 << WGM00) | (1 << WGM01); // Fast PWM
-    TCCR0A |= (1 << COM0A1); // Nicht-invertierend auf OC0A
-    TCCR0A |= (1 << COM0B1); // Nicht-invertierend auf OC0A
-    TCCR0B |= (1 << CS01) | (1 << CS00);  // Prescaler 64
-    OCR0A = MIN_BR;  // 50% Duty Cycle
-    OCR0B = HOUR_BR;
-}
-
-void goToSleep() {
+void setPWRDown() {
     set_sleep_mode(SLEEP_MODE_PWR_SAVE);
     sleep_enable();
     sleep_cpu();
-    sleep_disable();
 }
 
 int main() {
-    init_buttons();
-    init_timer();
-    initPWM();
     cli(); // interrupts deaktivieren
-
-    DDRC |= 0b00111111; // minuten
-    DDRD |= 0b00011111; // stunden
-    DDRD |= (1<<PD7);
-
+    initPWM();
+    initQuartz();
+    initGPIO();
+    initButtons();
     sei(); // interrupts aktivieren
 
-    //goToSleep();
+    initSleep();
 
     while (1) {
-        //sleep_mode();
-        //_delay_ms(500);
+        // hier testen wie oft die schleife iteriert wird?
+        // sollte nicht so oft sein. Eigentlich nur bei einem interrupt
+        // PORTD ^= (1<<PD4); // Jede Sekunde aufgerufen
+        sleep_mode();
     }
+    return 0;
 }
+
+
+
+/*
+ *  Button gedrückt / eine Minute vergangen:
+ *  LEDs aktualisieren
+ */
+void handleMinutes();
+void refreshLEDs_min();
+void refreshLEDs_hours();
 
 void handleTimeChange() {
     handleMinutes();
@@ -109,75 +130,61 @@ void handleMinutes() {
 void handleHours() {
     if (hours >= 24) {
         hours = 0;
-        PORTD |=(1<<PD7);
     }
     refreshLEDs_hours();
 }
 
-void refreshLEDs_min() {
-    for (int i=0; i<8; i++) {
-        int minVal = (minutes >> i) & 0b1;
+/* ---------------------- */
 
-        if (minVal) {
-            PORTC |= (1 << i);
-        } else {
-            PORTC &= ~(1 << i);
-        }
+
+
+void btnPressedStandardMode() {
+    if (!PB0_ps && PB0_cs) {
+        PORTC ^=(1<<PC1);
     }
 }
 
-void refreshLEDs_hours() {
-    for (int i=0; i<5; i++) {
-        int hoursVal = (hours >> i) & 0b1;
+void btnPressedExperimentMode() {
 
-        if (hoursVal) {
-            PORTD |= (1 << i);
-        } else {
-            PORTD &= ~(1 << i);
-        }
-    }
 }
 
+// eine Sekunde vergangen
 ISR(TIMER2_OVF_vect) {
-    seconds++;
-    PORTD ^= (1<<PD7);
-    if (seconds >= 60) {
-        seconds = 0;
-        minutes++;
-        handleTimeChange();
+    PORTD ^= (1<<PD3);
+    btnPressedTimer++;
+    if (btnPressedTimer >= 10) {
+        btnPressedTimer = 0;
     }
 }
 
+// Button wurde gedrückt
 ISR(PCINT0_vect) {
-    uint8_t pb0_interrupt = (PINB & 0b1);
-    uint8_t pb1_interrupt = (PINB >> 1) & 0b1;
-    uint8_t pb2_interrupt = (PINB >> 2) & 0b1;
+    PB0_cs = (PINB & 0b1);
+    PB1_cs = (PINB >> 1) & 0b1;
+    PB2_cs = (PINB >> 2) & 0b1;
 
-    // MINUTES
-    if (!pb0_interrupt && !previousRisingEdge_pb0) {
-        seconds = 0;
-        minutes++;
-        handleTimeChange();
 
-        previousRisingEdge_pb0 = pb0_interrupt;
+    /*
+     * Prüfen ob ein Zustandswechsel gemacht werden soll
+     * (PB0 wird mehr als 2 s gehalten
+     */
+    if ( PB0_ps && !PB0_cs) { // Button wurde runter gedrückt
+        btnPressedTimer = 0;
     }
-    // HOURS
-    else if (!pb1_interrupt && !previousRisingEdge_pb1) {
-        seconds = 0;
-        hours++;
-        handleHours();
-
-        previousRisingEdge_pb1 = pb1_interrupt;
+    else if ( !PB0_ps && PB0_cs ){ // Button wurde losgelassen
+        if (btnPressedTimer >= 2) { // Modi wechseln
+            PORTC ^= (1 << PC4);
+            // changeMode();
+        } else {
+            if (IN_STANDARD_MODI) {
+                btnPressedStandardMode();
+            } else {
+                btnPressedExperimentMode();
+            }
+        }
     }
-    // RESET
-    else if (!pb2_interrupt && !previousRisingEdge_pb2) {
-        PORTD ^= (1<<PD7);
-        seconds = 0;
-        minutes = 0;
-        hours = 0;
-        refreshLEDs_min();
-        refreshLEDs_hours();
 
-        previousRisingEdge_pb2 = pb2_interrupt;
-    }
+    PB0_ps = PB0_cs;
+    PB1_ps = PB1_cs;
+    PB2_ps = PB2_cs;
 }
